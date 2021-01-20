e8c6b3b8d6ffb5f129ced0af9ca683d5f9603c2f
Jan Ziemba <ziemba.jan@icloud.com>
2021-01-20 20:04:08 +0100

    test: fix lint errors

--- src/components/PasswordInput/index.tsx
+++ src/components/PasswordInput/index.tsx
@@ -16,92 +16,92 @@ import injectTheme, { Styles } from './styles';
 
 interface Props extends TextInputProps {}
 
-const PasswordInput: React.ForwardRefExoticComponent<Props & React.RefAttributes<RNTextInput>> = React.forwardRef<RNTextInput, Props>(
-    (props: Props, ref?: React.Ref<RNTextInput>) => {
-        const { allowFontScaling = false, autoCapitalize = 'none', onBlur, onFocus } = props;
+const PasswordInput: React.ForwardRefExoticComponent<
+    Props & React.RefAttributes<RNTextInput>
+> = React.forwardRef<RNTextInput, Props>((props: Props, ref?: React.Ref<RNTextInput>) => {
+    const { allowFontScaling = false, autoCapitalize = 'none', onBlur, onFocus } = props;
 
-        const styles: Styles = useStyles(injectTheme);
-        const { colors } = useTheme();
+    const styles: Styles = useStyles(injectTheme);
+    const { colors } = useTheme();
 
-        const [isEditable, setIsEditable] = React.useState(true);
-        const [isPasswordVisible, setIsPasswordVisible] = React.useState(false);
-        const [isFocused, setIsFocused] = React.useState(false);
+    const [isEditable, setIsEditable] = React.useState(true);
+    const [isPasswordVisible, setIsPasswordVisible] = React.useState(false);
+    const [isFocused, setIsFocused] = React.useState(false);
 
-        React.useEffect(() => {
-            // there is a bug in RN which doesn't let you paste a text
-            // https://github.com/facebook/react-native/issues/28366
-            // https://github.com/facebook/react-native/issues/20887
-            if (Platform.OS === 'android') {
-                setIsEditable(false);
+    React.useEffect(() => {
+        // there is a bug in RN which doesn't let you paste a text
+        // https://github.com/facebook/react-native/issues/28366
+        // https://github.com/facebook/react-native/issues/20887
+        if (Platform.OS === 'android') {
+            setIsEditable(false);
 
-                setTimeout(() => {
-                    setIsEditable(true);
-                }, 100);
+            setTimeout(() => {
+                setIsEditable(true);
+            }, 100);
+        }
+    }, []);
+
+    const handleFocus = React.useCallback(
+        (event: NativeSyntheticEvent<TextInputFocusEventData>) => {
+            setIsFocused(true);
+
+            if (onFocus) {
+                onFocus(event);
             }
-        }, []);
+        },
+        [onFocus, setIsFocused],
+    );
 
-        const handleFocus = React.useCallback(
-            (event: NativeSyntheticEvent<TextInputFocusEventData>) => {
-                setIsFocused(true);
+    const handleBlur = React.useCallback(
+        (event: NativeSyntheticEvent<TextInputFocusEventData>) => {
+            setIsFocused(false);
 
-                if (onFocus) {
-                    onFocus(event);
-                }
-            },
-            [onFocus, setIsFocused],
-        );
-
-        const handleBlur = React.useCallback(
-            (event: NativeSyntheticEvent<TextInputFocusEventData>) => {
-                setIsFocused(false);
-
-                if (onBlur) {
-                    onBlur(event);
-                }
-            },
-            [onBlur, setIsFocused],
-        );
-
-        const mergedTextInputStyles = React.useMemo((): ViewStyle[] => {
-            let result = [styles.textInput];
-
-            if (isFocused) {
-                result.push(styles.textInputFocused);
+            if (onBlur) {
+                onBlur(event);
             }
+        },
+        [onBlur, setIsFocused],
+    );
 
-            return result;
-        }, [isFocused, styles]);
+    const mergedTextInputStyles = React.useMemo((): ViewStyle[] => {
+        let result = [styles.textInput];
 
-        const handleIconPress = React.useCallback((): void => {
-            setIsPasswordVisible(!isPasswordVisible);
-        }, [isPasswordVisible]);
+        if (isFocused) {
+            result.push(styles.textInputFocused);
+        }
 
-        return (
-            <View style={styles.container}>
-                <RNTextInput
-                    {...props}
-                    allowFontScaling={allowFontScaling}
-                    autoCapitalize={autoCapitalize}
-                    editable={isEditable}
-                    onBlur={handleBlur}
-                    onFocus={handleFocus}
-                    placeholderTextColor={colors.disabled}
-                    ref={ref}
-                    secureTextEntry={!isPasswordVisible}
-                    style={mergedTextInputStyles}
-                />
-                <View style={styles.iconContainer}>
-                    <Scalable onPress={handleIconPress}>
-                        <Icon
-                            color={colors.success}
-                            iconSet="MaterialCommunityIcons"
-                            name={isPasswordVisible ? 'eye-off' : 'eye'}
-                        />
-                    </Scalable>
-                </View>
+        return result;
+    }, [isFocused, styles]);
+
+    const handleIconPress = React.useCallback((): void => {
+        setIsPasswordVisible(!isPasswordVisible);
+    }, [isPasswordVisible]);
+
+    return (
+        <View style={styles.container}>
+            <RNTextInput
+                {...props}
+                allowFontScaling={allowFontScaling}
+                autoCapitalize={autoCapitalize}
+                editable={isEditable}
+                onBlur={handleBlur}
+                onFocus={handleFocus}
+                placeholderTextColor={colors.disabled}
+                ref={ref}
+                secureTextEntry={!isPasswordVisible}
+                style={mergedTextInputStyles}
+            />
+            <View style={styles.iconContainer}>
+                <Scalable onPress={handleIconPress}>
+                    <Icon
+                        color={colors.success}
+                        iconSet="MaterialCommunityIcons"
+                        name={isPasswordVisible ? 'eye-off' : 'eye'}
+                    />
+                </Scalable>
             </View>
-        );
-    },
-);
+        </View>
+    );
+});
 
 export default PasswordInput;
--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -18,85 +18,85 @@ interface Props extends Omit<TextInputProps, 'editable'> {
     isDisabled?: boolean;
 }
 
-const TextInput: React.ForwardRefExoticComponent<Props & React.RefAttributes<RNTextInput>> = React.forwardRef<RNTextInput, Props>(
-    (props: Props, ref?: React.Ref<RNTextInput>) => {
-        const {
-            allowFontScaling = false,
-            autoCapitalize = 'none',
-            isDisabled = false,
-            onBlur,
-            onFocus,
-        } = props;
+const TextInput: React.ForwardRefExoticComponent<
+    Props & React.RefAttributes<RNTextInput>
+> = React.forwardRef<RNTextInput, Props>((props: Props, ref?: React.Ref<RNTextInput>) => {
+    const {
+        allowFontScaling = false,
+        autoCapitalize = 'none',
+        isDisabled = false,
+        onBlur,
+        onFocus,
+    } = props;
 
-        const styles: Styles = useStyles(injectTheme);
-        const { colors } = useTheme();
+    const styles: Styles = useStyles(injectTheme);
+    const { colors } = useTheme();
 
-        const [isEditable, setIsEditable] = React.useState(true);
-        const [isFocused, setIsFocused] = React.useState(false);
+    const [isEditable, setIsEditable] = React.useState(true);
+    const [isFocused, setIsFocused] = React.useState(false);
 
-        React.useEffect(() => {
-            // there is a bug in RN which doesn't let you paste a text
-            // https://github.com/facebook/react-native/issues/28366
-            // https://github.com/facebook/react-native/issues/20887
-            if (Platform.OS === 'android') {
-                setIsEditable(false);
+    React.useEffect(() => {
+        // there is a bug in RN which doesn't let you paste a text
+        // https://github.com/facebook/react-native/issues/28366
+        // https://github.com/facebook/react-native/issues/20887
+        if (Platform.OS === 'android') {
+            setIsEditable(false);
 
-                setTimeout(() => {
-                    setIsEditable(true);
-                }, 100);
+            setTimeout(() => {
+                setIsEditable(true);
+            }, 100);
+        }
+    }, []);
+
+    const handleFocus = React.useCallback(
+        (event: NativeSyntheticEvent<TextInputFocusEventData>) => {
+            setIsFocused(true);
+
+            if (onFocus) {
+                onFocus(event);
             }
-        }, []);
+        },
+        [onFocus, setIsFocused],
+    );
 
-        const handleFocus = React.useCallback(
-            (event: NativeSyntheticEvent<TextInputFocusEventData>) => {
-                setIsFocused(true);
+    const handleBlur = React.useCallback(
+        (event: NativeSyntheticEvent<TextInputFocusEventData>) => {
+            setIsFocused(false);
 
-                if (onFocus) {
-                    onFocus(event);
-                }
-            },
-            [onFocus, setIsFocused],
-        );
-
-        const handleBlur = React.useCallback(
-            (event: NativeSyntheticEvent<TextInputFocusEventData>) => {
-                setIsFocused(false);
-
-                if (onBlur) {
-                    onBlur(event);
-                }
-            },
-            [onBlur, setIsFocused],
-        );
-
-        const mergedTextInputStyles = React.useMemo((): ViewStyle[] => {
-            let result = [styles.textInput];
-
-            if (isFocused) {
-                result.push(styles.textInputFocused);
+            if (onBlur) {
+                onBlur(event);
             }
+        },
+        [onBlur, setIsFocused],
+    );
 
-            if (isDisabled) {
-                result.push(styles.textInputDisabled);
-            }
+    const mergedTextInputStyles = React.useMemo((): ViewStyle[] => {
+        let result = [styles.textInput];
 
-            return result;
-        }, [isDisabled, isFocused, styles]);
+        if (isFocused) {
+            result.push(styles.textInputFocused);
+        }
 
-        return (
-            <RNTextInput
-                {...props}
-                allowFontScaling={allowFontScaling}
-                autoCapitalize={autoCapitalize}
-                editable={!isDisabled && isEditable}
-                onBlur={handleBlur}
-                onFocus={handleFocus}
-                placeholderTextColor={colors.disabled}
-                ref={ref}
-                style={mergedTextInputStyles}
-            />
-        );
-    },
-);
+        if (isDisabled) {
+            result.push(styles.textInputDisabled);
+        }
+
+        return result;
+    }, [isDisabled, isFocused, styles]);
+
+    return (
+        <RNTextInput
+            {...props}
+            allowFontScaling={allowFontScaling}
+            autoCapitalize={autoCapitalize}
+            editable={!isDisabled && isEditable}
+            onBlur={handleBlur}
+            onFocus={handleFocus}
+            placeholderTextColor={colors.disabled}
+            ref={ref}
+            style={mergedTextInputStyles}
+        />
+    );
+});
 
 export default TextInput;
