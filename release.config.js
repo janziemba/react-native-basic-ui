314d9e464021826d87d12b2c71a77b38ca3c8614
Jan Ziemba <ziemba.jan@icloud.com>
2021-01-21 21:01:07 +0100

    chore: setup automatic releases with semantic-release and Github Actions

---
+++ release.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    branches: ['master'],
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        [
+            '@semantic-release/git',
+            {
+                message: 'chore(release): ${nextRelease.version}\n\n${nextRelease.notes}',
+            },
+        ],
+    ],
+};
