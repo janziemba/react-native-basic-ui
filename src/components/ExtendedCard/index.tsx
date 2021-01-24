baab88f94ceb4520350ea136e9ce23f7f066be20
Jan Ziemba <ziemba.jan@icloud.com>
2021-01-24 18:25:30 +0100

    test: fix lint error

--- src/components/ExtendedCard/index.tsx
+++ src/components/ExtendedCard/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { GestureResponderEvent, Image, ImageProps, View } from 'react-native';
+import { Image, ImageProps, View } from 'react-native';
 
 import { useStyles } from '../../theme';
 import Label from '../Label';
