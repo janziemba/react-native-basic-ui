3edfebc1ebd9b5cbc48b03758756f529526ca774
Jan Ziemba <ziemba.jan@icloud.com>
2021-01-24 18:38:22 +0100

    fix: fix compatibility with Expo - linear gradient

--- src/components/LinearGradient/index.expo.tsx
+++ src/components/LinearGradient/index.expo.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 // @ts-ignore
-import ExpoLinearGradient, { LinearGradientProps } from 'expo-linear-gradient';
+import { LinearGradient as ExpoLinearGradient, LinearGradientProps } from 'expo-linear-gradient';
 
 const LinearGradient: React.FunctionComponent<LinearGradientProps> = (
     props: LinearGradientProps,
